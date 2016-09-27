066621045ba754e9c6e5567e4db1d17ec6c5b897
tangert <tyler@angert.com>
2016-09-27 13:57:22 -0400

    fixed code-class issue

--- main.js
+++ main.js
@@ -18,78 +18,17 @@ $(document).ready(function(){
         $(".data-structures").hide();
     });
     
-    var alreadySelected = false;
-    var clickCount = 0;
-    
     $("#languages li a").click(function(){
   
-        var previousLanguage;
-        var currentLanguage = "language-" + this.id;
+        event.preventDefault();
+        var currentLanguage = "language-" + this.id + " text-center";
+  
+        //changes the language of the code-display block.
+        $('#chosen-language').removeClass();  
+        $('#chosen-language').addClass(currentLanguage);
         
-        //handles first time events 
-        if (alreadySelected == false) {
-            
-            $('#chosen-language').addClass(currentLanguage); 
-            $('#chosen-language').replaceWith(currentLanguage); 
-            alreadySelected = true;
-            
-            previousLanguage = currentLanguage;
-            
-            console.log("Current language: "+ currentLanguage);
-                        
-        } else {
-            
-            $('#chosen-language').removeClass(previousLanguage);
-            $('#chosen-language').addClass(currentLanguage)
-            previousLanguage = currentLanguage;
-            
-            console.log("Current language: "+ currentLanguage);
-            console.log("Previous language: "+ previousLanguage);
-            
-        }
-        
-        //select current language, good
-        //second time press, new current language, then previous current language = previous language
-        //remove previous language
-        //add new current language 
-        
+        //adjust language of data/structure/algorithm searching tool.
     });
-
-//    if chosen language == java,python,etc, 
-//        access certain file
-//    if chosen d-s == stack, queue, etc
-//        access div from certain file
-//     if chosen alg == mergesort, quick sort
-//    
     
     
 });
-
-    //initializing the chosen data points
-    var chosenLanguage;
-    var chosenDataStructure;
-    var chosenAlgorithm;
-
-    //function that loads a specific language into the code canvas.
-    
-    var languageIsSelected = false;
-//    
-//    $("#languages>li>a").click(function(){
-//        chosenLanguage = this.id;
-//        languageIsSelected = true;
-//    })
-//    
-//    if (languageIsSelected == true) {
-//        $("#data-structures>li>a").click(function(){
-//            chosenDataStructure = this.id;
-//        })
-//
-//        $("#algorithms>li>a").click(function(){
-//            chosenAlgorithm = this.id;
-//        })
-//    } else if (languageIsSelected == false {
-//               
-//        $("#languageSelectionWarning").fadeIn();
-//
-//    }
-//    
