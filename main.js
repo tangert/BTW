b0173ded1bba9272dbc2782e0bde224080b3ef6c
tangert <tyler@angert.com>
2016-09-27 23:06:43 -0400

    some updates

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 $(function(){
     $(".data-structures").hide();
     $(".algorithms").hide();
-    $(".linked-list-variations").hide();
-    $("#languageSelectionWarning").hide();
 });
 
 $(document).ready(function(){
     
+    var currentLanguage;
+    var chosenLanguage; 
+
     $("#dataStructuresBtn").click(function(){
         $(".data-structures").fadeIn();
         $(".algorithms").hide();
@@ -18,17 +19,93 @@ $(document).ready(function(){
         $(".data-structures").hide();
     });
     
+    
     $("#languages li a").click(function(){
   
         event.preventDefault();
-        var currentLanguage = "language-" + this.id + " text-center";
+        currentLanguage = this.id;
   
         //changes the language of the code-display block.
-        $('#chosen-language').removeClass();  
-        $('#chosen-language').addClass(currentLanguage);
+        $('#code-language').removeClass();  
+        $('#code-language').addClass(currentLanguage);
         
-        //adjust language of data/structure/algorithm searching tool.
+        //if a data structure or algorithm is selected
+        //update the data structure with the selected language 
+        
+        
+        
+        
     });
     
+    //helper methods
+    function mapLanguageToDataStructure() {     
+
+    }
+    
+    function mapLanguageToAlgorithm() {
+        
+    }
+    
+    function updateDataStructure() {
+        
+    }
+    
+    function updateAlgorithm() {
+        
+    }
+        
+    var selectDataStructure = $("#data-structures li a").click(function(){
+        
+        event.preventDefault();
+        
+        if (currentLanguage == "java") {
+            
+            chosenLanguage = "java";
+            
+            switch(this.id) {
+                case "linkedlist":  
+                case "stack":
+                case "queue":
+                case "tree":
+                case "graph":
+                case "dictionary": 
+                default:
+        }
+        
+        }
+        
+        //add cases for each language, then direct function to appropriate data structure and language. 
+        
+    });
+    
+    selectDataStructure();
+    
+    
+    var selectAlgorithm = $("#algorithms li a").click(function(){
+        
+        event.preventDefault();
+        
+        if (currentLanguage == "hljs java") {
+             
+            switch(this.id) {
+                case "breadth-first-search":
+                case "depth-first-search":
+                case "linear-search":
+                case "binary-search":
+                case "heap-sort":
+                case "insertion-sort": 
+                case "merge-sort":
+                case "quick-sort":
+                case "selection-sort":
+                case "bubble-sort":  
+                default:
+            }
+        }
+        
+        //add cases for each language, then direct function to appropriate data structure and language. 
+        
+    }); 
+    
+    selectAlgorithm();
     
 });
